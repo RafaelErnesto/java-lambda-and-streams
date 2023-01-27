524b8176a697501b2628a6ad1cffce9fe67ebdba
Rafael <rflernesto@gmail.com>
2023-01-26 21:12:18 -0400

    feat: joining and skip

--- src/main/java/streams/Main.java
+++ src/main/java/streams/Main.java
@@ -9,6 +9,7 @@ public class Main {
         List<String> firstNames = List.of("John", "Marie", "Bob", "Amanda", "Will", "James");
         List<String> lastNames = List.of("Cabbage", "Carrot", "Tomato", "Lettuce", "Potato");
         Random rand = new Random();
+        //## MAP
         Collection<String> fullNames = firstNames
                 .stream()
                 .map(item -> item + " "+ lastNames.get(rand.nextInt(lastNames.size()))).collect(Collectors.toList());
@@ -16,6 +17,7 @@ public class Main {
         System.out.println("Full names");
         fullNames.forEach(item -> System.out.println(item));
 
+        //## FILTER
         Collection<String> filteredNames = fullNames
                 .stream()
                 .filter(item -> item.contains("Tomato"))
@@ -25,6 +27,7 @@ public class Main {
         filteredNames.forEach(item -> System.out.println(item));
 
 
+        //## GROUPINGBY
         Map<String, List<String>> namesGroupedByLastName = fullNames
                 .stream()
                 .collect(
@@ -36,9 +39,22 @@ public class Main {
             nameList.forEach(name -> System.out.println(name));
         });
 
+        //## REDUCE
         List<Integer> integers = List.of(1,2,3,4,5,6,7,8,9);
         int sum = integers.stream()
                 .reduce(0, (accumulator, element) -> accumulator += element);
         System.out.println("Sum of numbers: "+ sum);
+
+        //## SKIP AND LIMIT
+        integers.stream()
+                .skip(2)
+                .limit(5)
+                .forEach(e -> System.out.println(e));
+
+        //## JOINING
+        String joinedStringNumbers = integers.stream()
+                .map(e -> String.valueOf(e))
+                .collect(Collectors.joining(","));
+        System.out.println(joinedStringNumbers);
     }
 }
